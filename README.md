b60426334419407acd57577fb484fa99c7813484
Lukas Prochazka <prochazka.lu@gmail.com>
2024-03-14 17:06:18 +0100

    Update ESLint and Prettier configurations

--- README.md
+++ README.md
@@ -4,6 +4,3 @@
 2. `.gitignore`
 3. ESLint
 4. Project
-
-## Preflight Errors
-<img width="1019" alt="Screenshot 2023-04-12 at 10 14 40" src="https://user-images.githubusercontent.com/80746311/231395696-e3508d97-f20a-4775-9d75-a80d3ad008d2.png">
--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-import fs from 'node:fs'; // import fs module to read or write files
+import fs from 'node:fs';
 import client from 'node:https';
 
-const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/'; // the memepage Url
+const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 const folderPath = './memes';
 
 // fetch function for HTML & possibly more (the images)
 // translating response into text and saving it in responseText
-
 const response = await fetch(memeUrl);
 const responseText = await response.text();
 
 // defining function declaration for filtering the responseText for image urls by using regex
-
 function filterImageUrls(data) {
   const regEx = /src="https:\/\/api.*\.jpg\?width=300/g;
   let match;
@@ -34,25 +32,19 @@ function saveImage(urls, filepath) {
 }
 
 // creating a folder called meme
-
 if (!fs.existsSync(folderPath)) {
   // check if folder already exists
-  fs.mkdirSync(folderPath); // creating folder
+  fs.mkdirSync(folderPath);
 }
 
 // saving filtered image urls to an array
-
 const filteredUrls = await filterImageUrls(responseText);
 
-console.log(filteredUrls);
-
 // looping through images and assigning download location and
-
 let counter = 1;
 const dec = 0;
 
 for (let i = 0; i < filteredUrls.length; i++) {
-  // console.log(filteredUrls[i]);
   counter = i + 1;
 
   if (i < 9) {
